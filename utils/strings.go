c2220dae0500802c259e80c1adca5a1b9acf2036
OpenIM-Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-03-19 11:05:47 +0800

    feat: add strings function. (#26)
    * optimization: slice sub.
    
    * fix: rpc logger add more detail info.
    
    * feat: add msg gateway error.
    
    * refactor: separate functions with error containing message and error with only stack trace.
    
    * feat: add stdout and stderr info.
    
    * feat: add strings and sql log.
    
    * feat: add copy function.
    
    * feat: add strings function.

--- utils/strings.go
+++ utils/strings.go
@@ -125,3 +125,41 @@ func IsDuplicateStringSlice(arr []string) bool {
 	}
 	return false
 }
+
+// Get the intersection of two slices
+func IntersectString(slice1, slice2 []string) []string {
+	m := make(map[string]bool)
+	n := make([]string, 0)
+	for _, v := range slice1 {
+		m[v] = true
+	}
+	for _, v := range slice2 {
+		flag, _ := m[v]
+		if flag {
+			n = append(n, v)
+		}
+	}
+	return n
+}
+
+// Get the diff of two slices
+func DifferenceString(slice1, slice2 []string) []string {
+	m := make(map[string]bool)
+	n := make([]string, 0)
+	inter := IntersectString(slice1, slice2)
+	for _, v := range inter {
+		m[v] = true
+	}
+	for _, v := range slice1 {
+		if !m[v] {
+			n = append(n, v)
+		}
+	}
+
+	for _, v := range slice2 {
+		if !m[v] {
+			n = append(n, v)
+		}
+	}
+	return n
+}
