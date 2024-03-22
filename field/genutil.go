c393fab96c31ae1fa120c5db8a1ac08ad9c3182d
skiffer-git <44203734@qq.com>
2024-03-22 14:18:48 +0800

    reorganize the directory structure

--- field/genutil.go
+++ field/genutil.go
@@ -56,6 +56,11 @@ func ExitWithError(err error) {
 	fmt.Fprintf(os.Stderr, "%s exit -1: %+v\n", progName, err)
 	os.Exit(-1)
 }
+
+// GetProcessName retrieves the name of the currently running process.
+// It achieves this by parsing os.Args[0], which typically contains the full path to the program.
+// If os.Args[0] is empty or unset for some reason, the function returns an empty string.
+// Note: This function assumes that os.Args contains at least the program name. This is a safe assumption under normal circumstances.
 func GetProcessName() string {
 	args := os.Args
 	if len(args) > 0 {
