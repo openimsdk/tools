dd5025dddaf56637d8853247abdc049c9499ac02
Xinwei Xiong (cubxxw) <3293172751nss@gmail.com>
2024-03-22 11:59:57 +0800

    feat: save openim util base code

--- db/mongo/mgoutil.go
+++ db/mongo/mgoutil.go
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package mgoutil
+package mongo
 
 import (
 	"context"
+
 	"github.com/openimsdk/tools/errs"
 	"github.com/openimsdk/tools/pagination"
 	"go.mongodb.org/mongo-driver/mongo"
