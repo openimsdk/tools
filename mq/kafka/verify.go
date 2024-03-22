668cd9c21a6545085dbf545ea900048df683fcad
withchao <993506633@qq.com>
2024-03-22 14:39:14 +0800

    feat: kafka

---
+++ mq/kafka/verify.go
@@ -0,0 +1,22 @@
+package kafka
+
+import (
+	"context"
+	"github.com/IBM/sarama"
+	"github.com/openimsdk/tools/errs"
+)
+
+func CheckKafka(ctx context.Context, conf Config) error {
+	kfk, err := BuildConsumerGroupConfig(conf, sarama.OffsetNewest)
+	if err != nil {
+		return err
+	}
+	cli, err := sarama.NewClient(conf.Addr, kfk)
+	if err != nil {
+		return errs.WrapMsg(err, "addr", conf.Addr)
+	}
+	if err := cli.Close(); err != nil {
+		return errs.WrapMsg(err, "close kafka")
+	}
+	return nil
+}
