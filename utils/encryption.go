cdb4c292855002077684c1b04708d17ec5847133
Xinwei Xiong (cubxxw) <3293172751nss@gmail.com>
2024-03-22 12:14:58 +0800

    feat: save openim string typeutil base code

--- utils/encryption.go
+++ utils/encryption.go
@@ -23,12 +23,14 @@ import (
 	"github.com/openimsdk/tools/errs"
 )
 
+// Md5 returns the md5 hash of the input string.
 func Md5(s string, salt ...string) string {
 	h := md5.New()
 	h.Write([]byte(s))
 	if len(salt) > 0 {
 		h.Write([]byte(salt[0]))
 	}
+
 	cipher := h.Sum(nil)
 	return hex.EncodeToString(cipher)
 }
