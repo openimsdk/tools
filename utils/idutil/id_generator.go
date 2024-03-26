b4965fee086828cf3b89ce1b9886f8ae964a10e5
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-03-26 16:17:18 +0800

    feat: add err new replace errors.New.

--- utils/idutil/id_generator.go
+++ utils/idutil/id_generator.go
@@ -5,9 +5,15 @@ import (
 	"github.com/openimsdk/tools/utils/stringutil"
 	"github.com/openimsdk/tools/utils/timeutil"
 	"math/rand"
+	"strconv"
+	"time"
 )
 
 func GetMsgIDByMD5(sendID string) string {
 	t := stringutil.Int64ToString(timeutil.GetCurrentTimestampByNano())
 	return encrypt.Md5(t + sendID + stringutil.Int64ToString(rand.Int63n(timeutil.GetCurrentTimestampByNano())))
+}
+
+func OperationIDGenerator() string {
+	return strconv.FormatInt(time.Now().UnixNano()+int64(rand.Uint32()), 10)
 }
