e380aa881e4bd6fbaa13d02dd7e8889567bf1a96
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-03-26 21:12:58 +0800

    feat: add err new replace errors.New.

--- db/redisutil/doc.go
+++ db/redisutil/doc.go
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package redis // import "github.com/openimsdk/tools/redis"
+package redisutil // import "github.com/openimsdk/tools/redisutil"
