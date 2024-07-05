99aa1c0ca5b0decbd1453397193e05c3ebc7bd30
godlovegeekin <godlovegeekin@gmail.com>
2024-07-05 10:28:06 +0800

    add time util function

--- utils/timeutil/time_format.go
+++ utils/timeutil/time_format.go
@@ -108,3 +108,145 @@ func TimeToString(t time.Time) string {
 func GetCurrentTimeFormatted() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
+
+// GetTimestampByTimezone get specific timestamp by timezone
+func GetTimestampByTimezone(timezone string) (int64, error) {
+	// set time zone
+	location, err := time.LoadLocation(timezone)
+	if err != nil {
+		return 0, errs.New("error loading location:", "error:", err)
+	}
+	// get current time
+	currentTime := time.Now().In(location)
+	// get timestamp
+	timestamp := currentTime.Unix()
+	return timestamp, nil
+}
+
+func DaysBetweenTimestamps(timezone string, timestamp int64) (int, error) {
+	// set time zone
+	location, err := time.LoadLocation(timezone)
+	if err != nil {
+		return 0, errs.New("error loading location:", "error:", err)
+	}
+	// get current time
+	now := time.Now().In(location)
+	// timestamp to time
+	givenTime := time.Unix(timestamp, 0)
+	// calculate duration
+	duration := now.Sub(givenTime)
+	// change to days
+	days := int(duration.Hours() / 24)
+	return days, nil
+}
+
+// IsSameWeekday judge current day and specific day is the same of a week.
+func IsSameWeekday(timezone string, timestamp int64) (bool, error) {
+	// set time zone
+	location, err := time.LoadLocation(timezone)
+	if err != nil {
+		return false, errs.New("error loading location:", "error:", err)
+	}
+	// get current weekday
+	currentWeekday := time.Now().In(location).Weekday()
+	// change timestamp to weekday
+	givenTime := time.Unix(timestamp, 0)
+	givenWeekday := givenTime.Weekday()
+	// compare two days
+	return currentWeekday == givenWeekday, nil
+}
+
+func IsSameDayOfMonth(timezone string, timestamp int64) (bool, error) {
+	// set time zone
+	location, err := time.LoadLocation(timezone)
+	if err != nil {
+		return false, errs.New("error loading location:", "error:", err)
+	}
+	// Get the current day of the month
+	currentDay := time.Now().In(location).Day()
+	// Convert the timestamp to time and get the day of the month
+	givenDay := time.Unix(timestamp, 0).Day()
+	// Compare the days
+	return currentDay == givenDay, nil
+}
+
+func IsWeekday(timestamp int64) bool {
+	// Convert the timestamp to time
+	givenTime := time.Unix(timestamp, 0)
+	// Get the day of the week
+	weekday := givenTime.Weekday()
+	// Check if the day is between Monday (1) and Friday (5)
+	return weekday >= time.Monday && weekday <= time.Friday
+}
+
+func IsNthDayCycle(timezone string, startTimestamp int64, n int) (bool, error) {
+	// set time zone
+	location, err := time.LoadLocation(timezone)
+	if err != nil {
+		return false, errs.New("error loading location:", "error:", err)
+	}
+	// Parse the start date
+	startTime := time.Unix(startTimestamp, 0)
+	if err != nil {
+		return false, errs.New("invalid start date format:", "error:", err)
+	}
+	// Get the current time
+	now := time.Now().In(location)
+	// Calculate the difference in days between the current time and the start time
+	diff := now.Sub(startTime).Hours() / 24
+	// Check if the difference in days is a multiple of n
+	return int(diff)%n == 0, nil
+}
+
+// IsNthWeekCycle checks if the current day is part of an N-week cycle starting from a given start timestamp.
+func IsNthWeekCycle(timezone string, startTimestamp int64, n int) (bool, error) {
+	// set time zone
+	location, err := time.LoadLocation(timezone)
+	if err != nil {
+		return false, errs.New("error loading location:", "error:", err)
+	}
+
+	// Get the current time
+	now := time.Now().In(location)
+
+	// Parse the start timestamp
+	startTime := time.Unix(startTimestamp, 0)
+	if err != nil {
+		return false, errs.New("invalid start timestamp format:", "error:", err)
+	}
+
+	// Calculate the difference in days between the current time and the start time
+	diff := now.Sub(startTime).Hours() / 24
+
+	// Convert days to weeks
+	weeks := int(diff) / 7
+
+	// Check if the difference in weeks is a multiple of n
+	return weeks%n == 0, nil
+}
+
+// IsNthMonthCycle checks if the current day is part of an N-month cycle starting from a given start timestamp.
+func IsNthMonthCycle(timezone string, startTimestamp int64, n int) (bool, error) {
+	// set time zone
+	location, err := time.LoadLocation(timezone)
+	if err != nil {
+		return false, errs.New("error loading location:", "error:", err)
+	}
+
+	// Get the current date
+	now := time.Now().In(location)
+
+	// Parse the start timestamp
+	startTime := time.Unix(startTimestamp, 0)
+	if err != nil {
+		return false, errs.New("invalid start timestamp format:", "error:", err)
+	}
+
+	// Calculate the difference in months between the current time and the start time
+	yearsDiff := now.Year() - startTime.Year()
+	monthsDiff := int(now.Month()) - int(startTime.Month())
+	totalMonths := yearsDiff*12 + monthsDiff
+
+	// Check if the difference in months is a multiple of n
+	return totalMonths%n == 0, nil
+}
