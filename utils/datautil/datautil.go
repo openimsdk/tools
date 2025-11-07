fd64f76012bfe6e56e3526ec44085a786bc79a0c
mo3et <monet_lee@163.com>
2025-11-07 11:48:57 +0800

    chore: remove unused comments.

--- utils/datautil/datautil.go
+++ utils/datautil/datautil.go
@@ -1,17 +1,3 @@
-// Copyright © 2023 OpenIM. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package datautil
 
 import (
@@ -481,24 +467,6 @@ func BothExist[E comparable](es ...[]E) []E {
 	})
 }
 
-//func CompleteAny[K comparable, E any](ks []K, es []E, fn func(e E) K) bool {
-//	if len(ks) == 0 && len(es) == 0 {
-//		return true
-//	}
-//	kn := make(map[K]uint8)
-//	for _, e := range Distinct(ks) {
-//		kn[e]++
-//	}
-//	for k := range SliceSetAny(es, fn) {
-//		kn[k]++
-//	}
-//	for _, n := range kn {
-//		if n != 2 {
-//			return false
-//		}
-//	}
-//	return true
-//}
 
 // Complete whether a and b are equal after deduplication (ignore order)
 func Complete[E comparable](a []E, b []E) bool {
